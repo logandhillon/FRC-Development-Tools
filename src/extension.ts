178b028e320f60c72b735a07df0e79ae76c933d1
Logan Dhillon <logandhi64@gmail.com>
2023-11-26 02:04:25 -0500

    Concise commands

--- src/extension.ts
+++ src/extension.ts
@@ -5,9 +5,9 @@ import * as vscode from 'vscode';
  * Your extension is activated the very first time the command is executed
  */
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Extension "ctre-docs-for-vs-code" is now active');
+	console.log('Extension "frc-devtools" is now active');
 	
-	let disposable = vscode.commands.registerCommand('ctre-docs-for-vs-code.openWebsite', () => {
+	let disposable = vscode.commands.registerCommand('frc-devtools.openCtreDocs', () => {
 		vscode.window.createWebviewPanel("ctreDocs", "CTRE Docs", vscode.ViewColumn.Active, {});
 	});
 
