6edde908aed4166b541c80e494d783ebb9218047
Logan Dhillon <logandhi64@gmail.com>
2023-11-26 16:02:27 -0500

    Show webpage as iframe

--- src/extension.ts
+++ src/extension.ts
@@ -8,10 +8,36 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Extension "frc-devtools" is now active');
 	
 	let disposable = vscode.commands.registerCommand('frc-devtools.openCtreDocs', () => {
-		vscode.window.createWebviewPanel("ctreDocs", "CTRE Docs", vscode.ViewColumn.Active, {});
+		let panel = vscode.window.createWebviewPanel("ctreDocs", "CTRE Docs", vscode.ViewColumn.Active, {enableScripts: true, enableCommandUris:true, enableForms:true});
+		panel.webview.html = getHtmlForWebpage("https://api.ctr-electronics.com/phoenix6/release/java/");
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
+
+function getHtmlForWebpage(url:string) {
+    return `
+        <!DOCTYPE html>
+        <html>
+        <head>
+			<style>
+			html, body {
+				height: 100%;
+				margin: 0;
+				padding: 0;
+			}
+			
+			iframe {
+				width: 100%;
+				height: 100%;
+			}
+			</style>
+        </head>
+        <body>
+            <iframe src="${url}" width="100%" height="100%"></iframe>
+        </body>
+        </html>
+    `;
+}
